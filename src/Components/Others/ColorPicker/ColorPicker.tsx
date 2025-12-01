3e71ed76acd4449e77c65099ab0aca1dab6b12a0
Shubham Kumar Singh <sksinghss1998@gmail.com>
2025-12-01 06:49:48 +0530

    fixing color picker bug

--- src/Components/Others/ColorPicker/ColorPicker.tsx
+++ src/Components/Others/ColorPicker/ColorPicker.tsx
@@ -62,7 +62,8 @@ const ColorPicker = ({ rgb, setRgb, darkTheme }: any) => {
                 alignItems: 'center', 
                 justifyContent: 'center', 
                 width: '100%', 
-                height: '100%' 
+                height: '100%',
+                minHeight: '300px'
             }}
         ></div>
     );
